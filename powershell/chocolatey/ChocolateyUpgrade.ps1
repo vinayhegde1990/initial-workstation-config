6061935d1cfb501b3f81d75c07911a180749614f
Vinay Hegde <vinay.hegde30@gmail.com>
2022-06-19 00:46:25 +0530

    Modify Chocolatey Upgrade Script
    Why this change?
    - Update OutdatedPackages to show limited output and filtered using ForEach-Object
    - Refer output variable to update only outdated pacakges
    
    Any relevant Links?
    - https://www.codegrepper.com/code-examples/shell/%22awk+-F+%27%27%22+powershell
    - https://stackoverflow.com/questions/10079572/powershell-equivalent-for-head-n-3
    - https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/foreach-object?view=powershell-7.2

--- powershell/chocolatey/ChocolateyUpgrade.ps1
+++ powershell/chocolatey/ChocolateyUpgrade.ps1
@@ -2,12 +2,12 @@
 
 function OutdatedPackages {
         Write-Host "Checking for outdated packages `n"
-        Invoke-Command -ScriptBlock {choco outdated --limit-output}
-        echo $?
+        Invoke-Command -ScriptBlock {choco outdated --limit-output | ForEach-Object{ $_.Split('|')[0]; } }
 }
+
 $output = OutdatedPackages
 function UpgradePackages {
-    Invoke-Command -ScriptBlock {choco upgrade all --yes}
+    Invoke-Command -ScriptBlock {choco upgrade $output --yes}
     Write-Host "Outdated packages upgraded via chocolatey"
 }
 
@@ -15,14 +15,5 @@ function CleanupTempFiles {
     Remove-Item -Recurse -Force 'C:\Users\Vinay\AppData\Local\Temp\chocolatey'
 }
 
-if ( $output -eq "True" )
-{
-  Write-Host "Upgrading outdated packages `n"
-  UpgradePackages
-
-  CleanupTempFiles
-  Write-Host "Deleted temp files to free up space `n"
-}
-else {
-  Write-Host "No new updates available, nothing to be done"
-}
+UpgradePackages
+CleanupTempFiles
